4aa2569c8c95972e5bc63eb4cf8d3fe9e283621a
Brian Quezada <briantpt30@gmail.com>
2018-10-31 15:53:32 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, redirect, render_template, session, flash
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/media')
+def media():
+    return render_template('media.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+

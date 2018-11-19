4d7513cca1b894ee222bc768af4272d611fa7e13
Brian Quezada <briantpt30@gmail.com>
2018-11-18 22:54:02 -0500

    tried email failed need help with security

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, redirect, render_template, session, flash
-
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT= 465,
+    MAIL_USE_SSL= True,
+    MAIL_USERNAME= 'briantpt30@gmail.com',
+    MAIL_PASSWORD='Casa1948'
+)
+mail = Mail(app)
+app.secret_key = 'y337kGcys&zP3B' 
 
 @app.route('/')
 def index():
@@ -17,8 +25,21 @@ def about():
 def media():
     return render_template('media.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
+
+    if request.method == 'POST':
+        sender = request.form['email']
+        body = request.form['body']
+
+        msg = Message(subject='test', recipients=['briantpt30@gmail.com'])
+        msg.body = body
+        mail.sender = sender
+        mail.send(msg)
+
+        return "Mail Sent"
+
+
     return render_template('contact.html')
 
 @app.route('/images')
